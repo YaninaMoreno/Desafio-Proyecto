fe0faaac9ddab25dd89f89282c59d0644130be80
Yanina Moreno <yani.elbolson@gmail.com>
2022-10-06 18:51:52 -0300

    Desafio proyecto

---
+++ js/array.js
@@ -0,0 +1,37 @@
+// const productos = [{
+//     id:1,
+//     nombre: "Nueces mariposa",
+//     precio: 1300,
+//     peso: "500gr",
+//     img:"../img/nuecesmariposa.jpg"},
+//     {
+//     id:2,
+//     nombre: "Almendras",
+//     precio: 1200,
+//     peso: "500gr",
+//     img:"../img/almendras.jpg"},
+//     {
+//     id:3,
+//     nombre: "Bananas deshidratadas",
+//     precio: 750,
+//     peso: "500gr",
+//     img:"../img/bananadisecada.jpg"},
+//     {
+//     id:4,
+//     nombre: "Almohaditas de avena",
+//     precio: 450,
+//     peso: "500gr",
+//     img:"../img/almohaditas.jpg"},
+//     {
+//     id:5,
+//     nombre: "Granola",
+//     precio: 550,
+//     peso: "500gr",
+//     img:"../img/granola.jpg"},
+//     {
+//     id:6,
+//     nombre: "Datiles",
+//     precio: 750,
+//     peso: "500gr",
+//     img:"../img/datiles.jpg"},
+// ];
